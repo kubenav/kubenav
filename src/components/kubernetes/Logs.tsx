677acd74d5d0042a2426c122d22c530a12371066
ricoberger <mail@ricoberger.de>
2020-01-25 23:07:09 +0100

    Improve logs handling
    By default only the last 1000 log lines are loaded, because the complete
    log results in very high traffic. This could be problematic on mobile.
    The complete log can be loaded via a new menu button in the logs view.
    
    It's now possible to load the previous log of a container. This should
    improve the debugging capabilities of kubenav. Like for the current log
    there are two options: Load the last 1000 log lines or load the complete
    log.
    
    To refresh the log, the corresponding menu item must be pressed again.
    The refresh button is gone in favour for options menu.

--- src/components/kubernetes/Logs.tsx
+++ src/components/kubernetes/Logs.tsx
@@ -5,14 +5,18 @@ import {
   IonContent,
   IonHeader,
   IonIcon,
+  IonItem,
   IonItemOption,
+  IonLabel,
+  IonList,
   IonModal,
+  IonPopover,
   IonProgressBar,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import { close, list, refresh } from 'ionicons/icons';
-import React, {useContext, useEffect, useState} from 'react';
+import { close, list, more } from 'ionicons/icons';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { AppContext } from '../../context';
 import { IContext } from '../../declarations';
@@ -31,23 +35,35 @@ const Logs: React.FunctionComponent<ILogsProps> = ({ name, namespace, container 
   const [showLoading, setShowLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
   const [logs, setLogs] = useState<string>('');
+  const [showPopover, setShowPopover] = useState<boolean>(false);
+  const [popoverEvent, setPopoverEvent] = useState();
 
   useEffect(() => {
     if (showModal) {
       (async() => {
         setLogs('');
-        await load();
+        await load(false, 1000);
       })();
     }
 
     return () => {};
   }, [showModal]); /* eslint-disable-line */
 
-  const load = async () => {
+  const load = async (previous: boolean, tailLines: number) => {
     setShowLoading(true);
 
     try {
-      const data: any = await context.request('GET', `/api/v1/namespaces/${namespace}/pods/${name}/log?container=${container}`, '');
+      let parameters = `container=${container}`;
+
+      if (previous) {
+        parameters = `${parameters}&previous=true`;
+      }
+
+      if (tailLines !== 0) {
+        parameters = `${parameters}&tailLines=${tailLines}`;
+      }
+
+      const data: any = await context.request('GET', `/api/v1/namespaces/${namespace}/pods/${name}/log?${parameters}`, '');
       setLogs(data);
     } catch (err) {
       setError(err);
@@ -74,9 +90,28 @@ const Logs: React.FunctionComponent<ILogsProps> = ({ name, namespace, container 
               </IonButton>
             </IonButtons>
             <IonTitle>{container}</IonTitle>
-            <IonButtons slot="primary" onClick={() => load()}>
-              <IonIcon slot="icon-only" icon={refresh} />
+            <IonButtons slot="primary">
+              <IonButton onClick={(e) => { e.persist(); setPopoverEvent(e); setShowPopover(true); }}>
+                <IonIcon slot="icon-only" icon={more} />
+              </IonButton>
             </IonButtons>
+
+            <IonPopover isOpen={showPopover} event={popoverEvent} onDidDismiss={() => setShowPopover(false)}>
+              <IonList>
+                <IonItem onClick={() => { setShowPopover(false); load(false, 1000); }}>
+                  <IonLabel>Log</IonLabel>
+                </IonItem>
+                <IonItem onClick={() => { setShowPopover(false); load(false, 0); }}>
+                  <IonLabel>Complete Log</IonLabel>
+                </IonItem>
+                <IonItem onClick={() => { setShowPopover(false); load(true, 1000); }}>
+                  <IonLabel>Previous Log</IonLabel>
+                </IonItem>
+                <IonItem onClick={() => { setShowPopover(false); load(true, 0); }}>
+                  <IonLabel>Complete Previous Log</IonLabel>
+                </IonItem>
+              </IonList>
+            </IonPopover>
           </IonToolbar>
         </IonHeader>
         <IonContent>
