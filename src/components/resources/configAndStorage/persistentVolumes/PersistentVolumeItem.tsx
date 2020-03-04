eef456fe47963150b60779b65238c2551e0ef0e3
ricoberger <mail@ricoberger.de>
2020-03-04 18:57:26 +0100

    Add status information for PV, PVC, CM, CRB and RB
    Add status information in the list view for PersistentVolumes,
    PersistentVolumeClaims, ConfigMaps, ClusterRoleBindings, RoleBindings
    and Secrets.

--- src/components/resources/configAndStorage/persistentVolumes/PersistentVolumeItem.tsx
+++ src/components/resources/configAndStorage/persistentVolumes/PersistentVolumeItem.tsx
@@ -6,6 +6,8 @@ import { V1PersistentVolume } from '@kubernetes/client-node'
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+import { timeDifference } from '../../../../utils/helpers';
+
 interface IPersistentVolumeItemProps extends RouteComponentProps {
   item: V1PersistentVolume;
   section: string;
@@ -13,10 +15,22 @@ interface IPersistentVolumeItemProps extends RouteComponentProps {
 }
 
 const PersistentVolumeItem: React.FunctionComponent<IPersistentVolumeItemProps> = ({ item, section, type }) => {
+  // - Phase: Indicates if a volume is available, bound to a claim, or released by a claim.
+  // - Capacity: Resources of the volume.
+  // - Access Modes: Contains all ways the volume can be mounted.
+  // - Reclaim Policy: What happens to a persistent volume when released from its claim.
+  // - Age: The time when the pv was created.
   return (
     <IonItem routerLink={`/resources/${section}/${type}/${item.metadata ? item.metadata.namespace : ''}/${item.metadata ? item.metadata.name : ''}`} routerDirection="forward">
       <IonLabel>
         <h2>{item.metadata ? item.metadata.name : ''}</h2>
+        <p>
+          Phase: {item.status && item.status.phase ? item.status.phase : '-'}
+          {item.spec && item.spec.capacity ? ` | Capacity: ${item.spec.capacity.storage}` : ''}
+          {item.spec && item.spec.accessModes ? ` | Access Modes: ${item.spec.accessModes.join(', ')}` : ''}
+          {item.spec && item.spec.persistentVolumeReclaimPolicy ? ` | Reclaim Policy: ${item.spec.persistentVolumeReclaimPolicy}` : ''}
+          {item.metadata && item.metadata.creationTimestamp ? ` | Age: ${timeDifference(new Date().getTime(), new Date(item.metadata.creationTimestamp.toString()).getTime())}` : ''}
+        </p>
       </IonLabel>
     </IonItem>
   )
--- src/components/resources/configAndStorage/secrets/SecretItem.tsx
+++ src/components/resources/configAndStorage/secrets/SecretItem.tsx
@@ -6,6 +6,8 @@ import { V1Secret } from '@kubernetes/client-node'
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+import { timeDifference } from '../../../../utils/helpers';
+
 interface ISecretItemProps extends RouteComponentProps {
   item: V1Secret;
   section: string;
@@ -13,10 +15,18 @@ interface ISecretItemProps extends RouteComponentProps {
 }
 
 const SecretItem: React.FunctionComponent<ISecretItemProps> = ({ item, section, type }) => {
+  // - Data: Number of data entries in the secret.
+  // - Type: Used to facilitate programmatic handling of secret data.
+  // - Age: The time when the secret was created.
   return (
     <IonItem routerLink={`/resources/${section}/${type}/${item.metadata ? item.metadata.namespace : ''}/${item.metadata ? item.metadata.name : ''}`} routerDirection="forward">
       <IonLabel>
         <h2>{item.metadata ? item.metadata.name : ''}</h2>
+        <p>
+          Data: {item.data ? Object.keys(item.data).length : 0}
+          {item.type ? ` | Type: ${item.type}` : ''}
+          {item.metadata && item.metadata.creationTimestamp ? ` | Age: ${timeDifference(new Date().getTime(), new Date(item.metadata.creationTimestamp.toString()).getTime())}` : ''}
+        </p>
       </IonLabel>
     </IonItem>
   )
