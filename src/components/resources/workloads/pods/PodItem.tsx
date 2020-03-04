987b64ccf1113f3de85a3a67723c1f0d47fe7236
ricoberger <mail@ricoberger.de>
2020-03-04 14:19:22 +0100

    Add status information for Pods
    Add a ready indicator which consists of number of containers which
    should are ready and number of containers. Add restart count and status.
    This should help users to quickly get an overview of problems within
    there pods.

--- src/components/resources/workloads/pods/PodItem.tsx
+++ src/components/resources/workloads/pods/PodItem.tsx
@@ -6,7 +6,9 @@ import { V1Pod } from '@kubernetes/client-node'
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+import { timeDifference } from '../../../../utils/helpers';
 import ItemStatus from '../../misc/template/ItemStatus';
+import { getReady, getRestarts, getStatus } from './podHelpers';
 
 interface IPodItemProps extends RouteComponentProps {
   item: V1Pod;
@@ -15,49 +17,32 @@ interface IPodItemProps extends RouteComponentProps {
 }
 
 const PodItem: React.FunctionComponent<IPodItemProps> = ({ item, section, type }) => {
-  const getPodStatus = (pod: V1Pod): string => {
-    if (pod.status && pod.status.phase === 'Failed') {
-      return 'danger';
-    }
+  // Get the status of the pod. If the status is running or completed we set the status to success.  If the pod stuck
+  // in the pending state, we set the status to warning. If none of these conditions applies we set the status to error.
+  const podStatus = getStatus(item);
+  let status = 'danger';
 
-    if (pod.status && pod.status.phase === 'Pending') {
-      return 'warning';
-    }
+  if (podStatus === 'Pending') {
+    status = 'warning';
+  }
 
-    if (pod.status && pod.status.initContainerStatuses) {
-      for (let container of pod.status.initContainerStatuses) {
-        if (
-          container.state
-          && (container.state.waiting || (container.state.terminated && container.state.terminated.exitCode !== 0))
-        ) {
-          return 'danger';
-        }
-      }
-    }
+  if (podStatus === 'Running' || podStatus === 'Completed') {
+    status = 'success';
+  }
 
-    if (pod.status && pod.status.containerStatuses) {
-      for (let container of pod.status.containerStatuses) {
-        if (
-          container.state
-          && (container.state.waiting || (container.state.terminated && container.state.terminated.exitCode !== 0))
-        ) {
-          return 'danger';
-        }
-      }
-
-      return 'success';
-    }
-
-    return '';
-  };
-
-  let status = getPodStatus(item);
-
+  // - Ready: The number of ready containers and the number of containers in the pod.
+  // - Restarts: Number of restarts for the pod, using the sum of restarts for all containers.
+  // - Status: Show the status of the pod, using the status of the running containers.
+  // - Age: The time when the daemon set was created.
   return (
     <IonItem routerLink={`/resources/${section}/${type}/${item.metadata ? item.metadata.namespace : ''}/${item.metadata ? item.metadata.name : ''}`} routerDirection="forward">
       <ItemStatus status={status} />
       <IonLabel>
         <h2>{item.metadata ? item.metadata.name : ''}</h2>
+        <p>
+          Ready: {getReady(item)} | Restarts: {getRestarts(item)} | Status: {podStatus}
+          {item.metadata && item.metadata.creationTimestamp ? ` | Age: ${timeDifference(new Date().getTime(), new Date(item.metadata.creationTimestamp.toString()).getTime())}` : ''}
+        </p>
       </IonLabel>
     </IonItem>
   )
