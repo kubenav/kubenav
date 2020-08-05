511e1942e68c3a8ed2adad6d4c6500a178f8c884
Robert O'Brien <robrien@redhat.com>
2020-08-05 23:13:54 +0100

    Minor grammatical changes

--- src/components/settings/ClustersPage.tsx
+++ src/components/settings/ClustersPage.tsx
@@ -40,10 +40,10 @@ const ClustersPage: React.FunctionComponent = () => {
           <IonCard>
             <IonCardContent>
               <p className="paragraph-margin-bottom">
-                It looks like you have not add a cluster yet. Click the button below to add your first cluster. You can
-                choose between different options when adding a cluster. You can import your cluster directly from one of
+                It looks like you have not added a cluster yet. Click the button below to add your first cluster. You
+                can choose between different options when adding a cluster. You can import your cluster directly from
                 your cloud providers like Google, AWS or Azure. You can also import your clusters from an existing
-                Kubeconfig or you can choose the OIDC option.
+                Kubeconfig file or you can choose the OIDC option.
               </p>
               <AddCluster activator="block-button" />
             </IonCardContent>
@@ -52,15 +52,15 @@ const ClustersPage: React.FunctionComponent = () => {
           <IonCard>
             <IonCardContent>
               <p className="paragraph-margin-bottom">
-                It looks like you have not configured a cluster yet. kubenav loads your clusters from{' '}
+                It looks like you have not configured a cluster yet. Kubenav loads your clusters from{' '}
                 <code>~/.kube/.kubeconfig</code>, <code>~/.kube/.config</code> or the <code>KUBECONFIG</code>{' '}
-                environment variable. You can also pass a Kubeconfig to kubenav via the <code>-kubeconfig</code> flag.
+                environment variable. You can also pass a Kubeconfig to Kubenav via the <code>-kubeconfig</code> flag.
               </p>
               <p>
                 With the <code>-include-kubeconfig</code> and <code>-exlude-kubeconfig</code> flags you can specify a
-                list of comma separated globs to load your Kubeconfig files from. The <code>-sync</code> flags wrotes
-                your cluster and namespace changes back to the loaded Kubeconfig, so that the context/namespace is also
-                changed in your terminal.
+                list of comma separated globs to load your Kubeconfig files from. The <code>-sync</code> flags writes
+                your cluster and namespace changes back to the loaded Kubeconfig file, so that the context/namespace is
+                also changed in your terminal.
               </p>
             </IonCardContent>
           </IonCard>
