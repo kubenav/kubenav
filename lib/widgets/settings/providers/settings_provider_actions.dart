21f324b9d84f06191eb258548e493bf941d30732
ricoberger <mail@ricoberger.de>
2024-05-26 12:43:34 +0200

    Add Swipe Actions for Providers
    The actions for providers in the settings can now be used by swiping a
    provider to the left. This will reveal a edit and delete action for the
    provider, which are the same actions as shown via the long press.

--- lib/widgets/settings/providers/settings_provider_actions.dart
+++ lib/widgets/settings/providers/settings_provider_actions.dart
@@ -15,12 +15,50 @@ import 'package:kubenav/widgets/settings/providers/settings_oidc_provider_config
 import 'package:kubenav/widgets/settings/providers/settings_rancher_provider_config.dart';
 import 'package:kubenav/widgets/shared/app_actions_widget.dart';
 
+/// [buildProviderModal] returns a widget which can be displayed in a modal,
+/// to modify the configuration of the provider given via the
+/// [provider] parameter.
+Widget buildProviderModal(ClusterProvider provider) {
+  switch (provider.type) {
+    case ClusterProviderType.aws:
+      return SettingsAWSProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.awssso:
+      return SettingsAWSSSOProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.azure:
+      return SettingsAzureProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.digitalocean:
+      return SettingsDigitalOceanProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.google:
+      return SettingsGoogleProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.oidc:
+      return SettingsOIDCProvider(
+        provider: provider,
+      );
+    case ClusterProviderType.rancher:
+      return SettingsRancherProvider(
+        provider: provider,
+      );
+    default:
+      return Container();
+  }
+}
+
 /// The [SettingsProviderActions] returns a list of actions to edit or delete an
 /// existing provider. If the user selects the edit item, we show a modal with
 /// the provider configuration, which can then be modified by the user. If the
 /// user selects the delete item, we remove the provider, when it is not used by
 /// a cluster.
-class SettingsProviderActions extends StatefulWidget {
+class SettingsProviderActions extends StatelessWidget {
   const SettingsProviderActions({
     super.key,
     required this.provider,
@@ -28,71 +66,27 @@ class SettingsProviderActions extends StatefulWidget {
 
   final ClusterProvider provider;
 
-  @override
-  State<SettingsProviderActions> createState() =>
-      _SettingsProviderActionsState();
-}
-
-class _SettingsProviderActionsState extends State<SettingsProviderActions> {
-  /// [buildProviderModal] returns a widget which can be displayed in a modal,
-  /// to modify the configuration of the provider given via the
-  /// [widget.provider] parameter.
-  Widget buildProviderModal() {
-    switch (widget.provider.type) {
-      case ClusterProviderType.aws:
-        return SettingsAWSProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.awssso:
-        return SettingsAWSSSOProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.azure:
-        return SettingsAzureProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.digitalocean:
-        return SettingsDigitalOceanProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.google:
-        return SettingsGoogleProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.oidc:
-        return SettingsOIDCProvider(
-          provider: widget.provider,
-        );
-      case ClusterProviderType.rancher:
-        return SettingsRancherProvider(
-          provider: widget.provider,
-        );
-      default:
-        return Container();
-    }
-  }
-
-  /// [deleteProvider] deletes the given provider. For the success and the error
-  /// case we show the user an message, to inform the user about the result of
-  /// the operation.
-  Future<void> deleteProvider() async {
+  /// [_deleteProvider] deletes the given provider. For the success and the
+  /// error case we show the user an message, to inform the user about the
+  /// result of the operation.
+  Future<void> _deleteProvider(BuildContext context) async {
     ClustersRepository clustersRepository = Provider.of<ClustersRepository>(
       context,
       listen: false,
     );
 
     try {
-      await clustersRepository.deleteProvider(widget.provider.id!);
-      if (mounted) {
+      await clustersRepository.deleteProvider(provider.id!);
+      if (context.mounted) {
         Navigator.pop(context);
         showSnackbar(
           context,
           'Provider Deleted',
-          'The provider ${widget.provider.name} was deleted',
+          'The provider ${provider.name} was deleted',
         );
       }
     } catch (err) {
-      if (mounted) {
+      if (context.mounted) {
         Navigator.pop(context);
         showSnackbar(
           context,
@@ -112,14 +106,14 @@ class _SettingsProviderActionsState extends State<SettingsProviderActions> {
           color: Theme.of(context).colorScheme.primary,
           onTap: () {
             Navigator.pop(context);
-            showModal(context, buildProviderModal());
+            showModal(context, buildProviderModal(provider));
           },
         ),
         AppActionsWidgetAction(
           title: 'Delete',
           color: Theme.of(context).extension<CustomColors>()!.error,
           onTap: () {
-            deleteProvider();
+            _deleteProvider(context);
           },
         ),
       ],
